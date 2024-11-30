b807aed4e555b0916461fdd69c6fea76b071cfa4
jwtiyar <jwtiyar@gmail.com>
2024-11-30 07:41:26 +0300

    Clear

--- project.py
+++ project.py
@@ -4,19 +4,16 @@ import calendar
 
 days = [i for i in range(1,32,1)]
 days = (','.join(map(str,days))).strip()
-
+header = ["Year", "Year", "Month", *range(1,32)]
+Year = input("Enter Year: ")
+Month = input("Enter Month")
 for i in range(4):
     finger1 = input("input the fingerprints: ")
-    finger = ''.join(finger1)
-year = input("Enter Year: ")
-month = input("Enter Month")
-field = ['fingerprints', 'month', 'year',days]
-
 def main():
     with open('shoft.csv', 'w', newline='') as file:
         add = csv.writer(file, quoting=csv.QUOTE_NONE, escapechar='/')
-        add.writerow(field)
-        add.writerows(finger)
+        add.writerow(header)
+        
     
 
 if __name__ == "__main__":
