e74e17f7a249f230deeb65524aef80eb53dc89ad
jwtiyar <jwtiyar@gmail.com>
2024-11-30 08:34:31 +0300

    New Ideas

--- project.py
+++ project.py
@@ -1,21 +1,27 @@
 import csv
-import datetime
+import datetime as dt
 import calendar
 
 days = [i for i in range(1,32,1)]
 days = (','.join(map(str,days))).strip()
-header = ["Year", "Year", "Month", *range(1,32)]
+header = ["Fingerprints", "Year", "Month", *range(1,32)]
 Year = input("Enter Year: ")
-Month = input("Enter Month")
-for i in range(4):
-    finger1 = input("input the fingerprints: ")
+Month = input("Enter Month: ")
 def main():
     with open('shoft.csv', 'w', newline='') as file:
         add = csv.writer(file, quoting=csv.QUOTE_NONE, escapechar='/')
         add.writerow(header)
-        
+        for i in range(4):
+            finger = input("input the fingerprints: ")
+            if i % 2:
+                dayCell1, dayCell2 = 6 , "x"
+            else:
+                dayCell1, dayCell2 = "x" , 6
+            for day in range(1,31):
+                if dt.datetime(Year,Month,day).weekday() == friday
+            cells = [finger,Month , Year, *([dayCell1,dayCell2]*15), dayCell1]
+            add.writerow(cells)
     
-
 if __name__ == "__main__":
     main()
 
---
+++ weekday.py
@@ -0,0 +1,3 @@
+import datetime as dt
+
+print(dt.datetime(2024,8,8).weekday())
